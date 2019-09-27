a514f73fb51a173ce7ef51e0079333cf0e37a920
Camila Teran <camyteran232@gmail.com>
2019-09-27 11:54:27 -0500

    NeatBean

---
+++ Workshops/ChickenFarmer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.chickenfarmer.view;
+
+import ec.edu.espe.chickenfarmer.model.Chicken;
+
+/**
+ *
+ * @author Labs-DCCO
+ */
+public class ChickenFarmer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Chicken chicken = new Chicken();
+        chicken.setName("Lucy");
+        chicken.dostuff(0);
+        Chicken chickens[];
+        
+        
+    }
+    
+}
