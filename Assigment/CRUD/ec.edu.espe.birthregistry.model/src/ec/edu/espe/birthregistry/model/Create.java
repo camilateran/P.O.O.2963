12c16e2e973fb80b5271d563fe553cffac76f67c
Camila Teran <camyteran232@gmail.com>
2019-09-27 22:13:20 -0500

    CRUD v0.2

--- Assigment/CRUD/ec.edu.espe.birthregistry.model/src/ec/edu/espe/birthregistry/model/Create.java
+++ Assigment/CRUD/ec.edu.espe.birthregistry.model/src/ec/edu/espe/birthregistry/model/Create.java
@@ -13,10 +13,76 @@ public class Create {
    private String name;
    private String lastnameoffahter;
    private String lastnameofmother;
-   private int date;
-   private int month;
-   private int year;
-   private int weight;
+   private String date;
+   private String month;
+   private String year;
+   private String weight;
+
+    public Create(String name, String lastnameoffahter, String lastnameofmother, String date, String month, String year, String weight) {
+        this.name = name;
+        this.lastnameoffahter = lastnameoffahter;
+        this.lastnameofmother = lastnameofmother;
+        this.date = date;
+        this.month = month;
+        this.year = year;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastnameoffahter() {
+        return lastnameoffahter;
+    }
+
+    public void setLastnameoffahter(String lastnameoffahter) {
+        this.lastnameoffahter = lastnameoffahter;
+    }
+
+    public String getLastnameofmother() {
+        return lastnameofmother;
+    }
+
+    public void setLastnameofmother(String lastnameofmother) {
+        this.lastnameofmother = lastnameofmother;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
    
    
 
--- Assigment/CRUD/ec.edu.espe.birthregistry.model/src/ec/edu/espe/birthregistry/model/Registry.java
+++ Assigment/CRUD/ec.edu.espe.birthregistry.model/src/ec/edu/espe/birthregistry/model/Registry.java
@@ -5,7 +5,15 @@
  */
 package ec.edu.espe.birthregistry.model;
 
+/*
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+*/
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,35 +29,52 @@ public class Registry {
     public static void main(String[] args) {
         System.out.println("---Birth Registry---");
         Scanner entrada = new Scanner(System.in);
-        int opcion;
-        System.out.println("1. Registrarse");
-        System.out.println("1. Registrarse");
-        System.out.println("1. Registrarse");
-        System.out.println("1. Registrarse");
-        
-        opcion = entrada.nextInt();
-        switch (opcion){
-            case 1:
-              Create();
-                break;
-            case 2:
-                Read();
-                break;
-            case 3:
-                Update();
-                break;
-            case 4:
-                Delete();
-                break;
-            default:
-                break;
-        }
-                
-        
+        List<Create> Registry= new ArrayList<Create>();
+        Registry.add(new Create ("Daniela", "Lopez", "Sandoval","27", "Septiembre", "2019", "3.4 kg"));
+        ExportarCSV(Registry);
     }
     public static void ExportarCSV(List<Create> create){
         String salidaArchivo = "Registros.csv";
         boolean existe = new File(salidaArchivo).exists();
+        if (existe){
+            File archivoRegistro = new File(salidaArchivo);
+            archivoRegistro.delete();
+        }
+        try {
+            CsvWriter salidaCSV = new CsvWriter(new FileWriter(salidaArchivo, true));
+        salidaCSV.write("Nombre ");
+        salidaCSV.write("Apellido paterno ");
+        salidaCSV.write("Apellido materno ");
+        salidaCSV.write("Día ");
+        salidaCSV.write("Mes ");
+        salidaCSV.write("Año ");
+        salidaCSV.write("Peso ");
+        
+        salidaCSV.endRecord();
+     
+        for (Create user: create){
+            salidaCSV.write(user.getName());
+            salidaCSV.write(user.getLastnameoffahter());
+            salidaCSV.write(user.getLastnameofmother());
+            salidaCSV.write(user.getDate());
+            salidaCSV.write(user.getMonth());
+            salidaCSV.write(user.getYear());
+            salidaCSV.write(user.getWeight());
+            
+            salidaCSV.endRecord();
+        }
+        
+        salidaCSV.close(); 
+            
+        } catch (IOException e){
+            e.printStackTrace();
+            
+        }
+        
+    }
+
+    private String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
