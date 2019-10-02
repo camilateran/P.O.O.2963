fbfa4bebb9ffe4ad462bad1501219c6ed3279251
Camila Teran <camyteran232@gmail.com>
2019-10-01 22:19:32 -0500

    Python CI

--- Assigment/HW_0.7_IdentificationCard.py
+++ Assigment/HW_0.7_IdentificationCard.py
@@ -1,21 +1,37 @@
+#CI:
+ 
+def validacionCI(ingreso):
+    longitud = len(ingreso)
+    if longitud == 10:
+        codeProvince = int(ingreso[0:2])
+        if codeProvince >= 1 and codeProvince <= 24:
+            thirdDigit = int(ingreso[2])
+            if thirdDigit >= 0 and thirdDigit < 6:
+                if longitud == 10:
+                    return CI(ingreso)
+            else:
+                raise Exception(u'Tercer digito invalido')
+        else:
+            raise Exception(u'Codigo de provincia incorrecto')
+    else:
+        raise Exception(u'Longitud incorrecta del numero ingresado')   
 
-while True:
-    try:
-        ingreso = input("Ingrese su cedula: ")
-        break
-    except ValueError:
-        print("Error, Ingrese los digitos de su cedula")
-
-par= ingreso[0], ingreso[2], ingreso[4], ingreso[6], ingreso[8]
-print(par)
-impar= ingreso[1], ingreso[3], ingreso[5], ingreso[7], ingreso[9]
-
-print(par[0])
-print(par[1])
-
-for i in par :
-   i=par*2
-   print(i)
-
-
-
+def CI(ingreso):
+    mult=(2,1,2,1,2,1,2,1,2)
+    sumaDigitos=0
+    cedula=int(ingreso)
+    for i in range(0,9):
+        multDigitos=int(ingreso[i])*(mult[i])
+        if (multDigitos >= 10):
+            multDigitos-=9
+        sumaDigitos += multDigitos
+        
+    decenaProxima = sumaDigitos+ (10-(sumaDigitos%10))
+    digitoVerificador= decenaProxima-sumaDigitos
+ 
+    if int(ingreso[9]) == digitoVerificador:
+        print("--Cedula valida--")
+    else:
+        print("--Cedula no valida--")
+#ingreso = input("Ingrese su cedula: ")        
+validacionCI(input("Ingrese su cedula-> "))   
