d965b135ff8b009b5a3730e7137a338e67724f36
Camila Teran <camyteran232@gmail.com>
2019-10-15 11:34:26 -0500

    Python

--- Workshops/python.py
+++ Workshops/python.py
@@ -1,4 +1,23 @@
 student= {
     'name': 'wladymir',
-    
+    'subjects':[
+        {
+            'name': 'Software',
+            'nrc':'2018'
+        },
+    ],
+    'university':{
+        'name':'ESPE',
+        'carrers': [
+            'sistemas',
+            'software',
+            'TI'
+        ]
+    }
 }
+def main_function(number):
+    def square_number(number):
+        return number**2
+
+    return square_number(number)
+
