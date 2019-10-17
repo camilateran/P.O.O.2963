8e602231865535c41f7df529eadb09dd07c9fbc2
Camila Teran <camyteran232@gmail.com>
2019-10-17 11:28:50 -0500

    JAVACLASS

---
+++ Workshops/JAVACLASS/Main.java
@@ -0,0 +1,57 @@
+public class Main{
+    public static void main(String[] args){
+        int x=5;
+        int y = X++;
+
+        //++ despues, version sufijo
+        //primero asigna luego aumenta
+
+        //++ antes version prefijo
+        //primero aumenta luego asigna
+
+           
+        
+    }
+    
+   /* public String method(){
+        
+        return "";
+    }
+        
+        /*for( int i=0; i<10; i++){
+            if (i==5){
+                /*ROMPE
+                continue;  
+            }
+            System.out.println(i);
+
+            break;
+        }
+        List<Integer> list= new ArrayList<Integer>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        for(int i: list){
+            System.out.println(i);
+        }
+       
+        /*float flt = 0.2f;
+        boolean bln= true;
+        String str= "";
+        double dbl= 20.36;
+        char chr= 'w';
+        char chr2= 56;
+
+        int i=5;
+        System.out.println(i==5 ? "Esto es 5":"Es otro numero");
+        CONSTRUCTOR
+        Person  person = new Person();
+        
+        System.out.println(person.getNum());
+        System.out.println(person.getStr());*/
+
+
+    
+}
---
+++ Workshops/JAVACLASS/Person.java
@@ -0,0 +1,31 @@
+public class Person{
+    private int num;
+    private String str;
+
+    
+    public Person(){
+        num = 10;
+
+    }
+    public Person(int num){
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    
+}
